b74874773c1c8493fb039b60a101153d996e603c
Dan <Bdylan998@gmail.com>
2021-01-21 15:08:42 +0200

    Lesson 7 Error Handling

--- lesson-7-ErrorHandling/lesson-7-ErrorHandling/lesson-7-ErrorHandling/main.swift
+++ lesson-7-ErrorHandling/lesson-7-ErrorHandling/lesson-7-ErrorHandling/main.swift
@@ -7,5 +7,42 @@
 
 import Foundation
 
+enum BankAccountError: Error {
+    case insuficientFunds
+    case invalidAmount
+}
 
+class BankAccount {
+    var amount = 0.0
+    
+    func depositFunds(funds: Double) throws -> Double {
+        if funds < 0 {
+            throw BankAccountError.invalidAmount
+        }
+        amount += funds
+        return amount
+    }
+    func withdrowFunds(funds: Double) throws -> Double {
+        if funds < 0 {
+            throw BankAccountError.invalidAmount
+        }
+        if funds > amount {
+            throw BankAccountError.insuficientFunds
+        }
+        amount -= funds
+        return amount
+    }
+}
 
+do {
+    var myBankAccount = BankAccount()
+    try myBankAccount.depositFunds(funds: -20.0)
+    print(myBankAccount.amount)
+} catch BankAccountError.insuficientFunds {
+    print("insuffucuent funds error")
+} catch BankAccountError.invalidAmount {
+    print("invalid amount error")
+}
+  catch {
+    print("an error has occured \(error.localizedDescription)")
+}
